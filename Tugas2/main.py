9647487cad96348a37223a599bbbe02ecb92b883
MUCH. MENTARI ADRIANSYAH <muchmenadriansyah@gmail.com>
2026-09-19 21:47:26 +0700

    Update main.py

--- Tugas2/main.py
+++ Tugas2/main.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from block import Block
 from pow import proof_of_work 
 from pos import proof_of_stake
@@ -8,21 +9,21 @@ from pos import proof_of_stake
 
 print("PROOF OF WORK")
 
-block = Block(
-    index=1,
-    data="Tiket dari Prmotor ke Venue",
-    previous_hash="0"
-)
+for difficulty in [2, 3, 4, 5]:
 
-difficulty = 2
+    block = Block(
+        index=1,
+        data="Tiket dari Prmotor ke Venue",
+        previous_hash="0"
+    )
 
-print("\nData Block       :", block.data)
-print("Difficulty        :", difficulty)
+    print("\nData Block       :", block.data)
+    print("Difficulty        :", difficulty)
 
-proof_of_work(block, difficulty)
+    proof_of_work(block, difficulty)
 
-print("Nonce             :", block.nonce)
-print("Hash              :", block.hash)
+    print("Nonce             :", block.nonce)
+    print("Hash              :", block.hash)
 
 print("PROOF OF STAKE")
 
@@ -37,6 +38,10 @@ print("\nValidator:")
 for validator, stake in validators.items():
     print(f"- {validator}: {stake} stake")
 
-selected = proof_of_stake(validators)
+hasil = []
+for i in range(1, 21):
+    selected = proof_of_stake(validators)
+    print(f"{i:>2}. Validator terpilih:", selected)
+    hasil.append(selected)
 
-print("\nValidator terpilih:", selected)
+print("\nRekap:", Counter(hasil))
